4935ef5145e793b5eacd56a564a457af260e5702
Xinbo Zong <zongxinbo@gmail.com>
2017-06-23 10:39:04 +0800

    Update js-utils.js

--- js-utils.js
+++ js-utils.js
@@ -1,5 +1,5 @@
 function getUrlParam(key, url) {
 	url = url ? url : window.location.search;
-	var result = url.match(new RegExp(key + "=([^&]*)"))
+	var result = url.match(new RegExp(key + "=([^&]*)"));
 	return result ? decodeURIComponent(result[1]) : "";
 }
